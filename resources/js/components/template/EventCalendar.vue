b544a55fdda5f2c47ec858fd930f0ded07e902aa
Gene M <eug5ne@gmail.com>
2019-08-11 15:42:51 +0800

    Calendar styling

--- resources/js/components/template/EventCalendar.vue
+++ resources/js/components/template/EventCalendar.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <h3>{{getMonthName()}} {{date.getFullYear()}}</h3>
+    <div class="calendar-dates" v-bind:style="{height: windowHeight}">
+      <div v-for="d in daysInMonth" :key="d">
+        <div class="col-md-2">
+          <p>{{d}} {{getDay(d)}}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,8 +15,20 @@
 export default {
   data() {
     return {
-      date: new Date()
+      date: new Date(),
+      daysInMonth: null,
+      windowHeight: 300
     };
+  },
+  created() {
+    window.addEventListener("resize", this.handleResize);
+    this.handleResize();
+  },
+  beforeMount() {
+    // get number of days in the selected month
+    var year = this.date.getFullYear();
+    var month = this.date.getMonth();
+    this.daysInMonth = new Date(year, month + 1, 0).getDate();
   },
   mounted() {
     // console.log("Component mounted.");
@@ -31,6 +50,25 @@ export default {
         "December"
       ];
       return months[this.date.getMonth()];
+    },
+    getDay(d) {
+      var days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+      ];
+
+      var year = this.date.getFullYear();
+      var month = this.date.getMonth();
+      var dt = new Date(year, month, d);
+      return dt.toString().split(" ")[0];
+    },
+    handleResize() {
+      this.windowHeight = window.innerHeight - 200 + "px";
     }
   }
 };
